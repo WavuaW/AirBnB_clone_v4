5dae1a850ab2eb534214a140a2b70b9d3395d543
dennisfofie <dennisbeckham000@gmail.com>
2023-02-23 19:18:19 +0000

    submit task 4

--- web_dynamic/static/scripts/4-hbnb.js
+++ web_dynamic/static/scripts/4-hbnb.js
@@ -49,5 +49,39 @@ $(document).ready(function(){
         }
 
     })
+
+    $('button').click(function() {
+        $.post({
+            url: "http://0.0.0.0:5001/api/v1/places_search/",
+            headers: {
+                "Content-Type": 'application/json'
+            },
+            data: JSON.stringify({"amenities": Object.keys(my_dict)}),
+            success: function(data) {
+                data.foreach(function(place) {
+                   $("section.places").html(`
+                   <article>
+                   <div class="title_box">
+                   <h2>{{ place.name }}</h2>
+                   <div class="price_by_night">$${ place.price_by_night }</div>
+                 </div>
+                 <div class="information">
+                   <div class="max_guest">{{ place.max_guest }} Guest{% if place.max_guest != 1 %}s{% endif %}</div>
+                       <div class="number_rooms">${ place.number_rooms } Bedroom{% if place.number_rooms != 1 %}s{% endif %}</div>
+                       <div class="number_bathrooms">${ place.number_bathrooms } Bathroom{% if place.number_bathrooms != 1 %}s{% endif %}</div>
+                 </div>
+                 <div class="user">
+                       <b>Owner:</b> ${ place.user.first_name } ${ place.user.last_name }
+                     </div>
+                     <div class="description">
+                   ${ place.description | safe }
+                     </div>
+                   </article>`) 
+                } )
+            }
+    
+        })
+        
+    })
     
 })
